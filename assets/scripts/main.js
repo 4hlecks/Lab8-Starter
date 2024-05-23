803011547f09bbae9fb695449dc1ec1e23d883e3
alatienza@ucsd.edu <alatienza@ucsd.edu>
2024-05-23 03:48:39 -0700

    fix sw.js scope

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,8 +45,7 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   async function registerServiceWorkers() {
-    const registration = await navigator.serviceWorker.register("/sw.js", {
-      scope: "./",});
+    const registration = await navigator.serviceWorker.register("/sw.js");
     if (registration.installing) {
       // B4. TODO - Once the service worker has been successfully registered, console
       //            log that it was successful.
